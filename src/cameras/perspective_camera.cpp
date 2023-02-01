b96528b4902853860ea3b746d2a7add4a37595f2
leiglat1 <leiglat1@FST-O-I-213-14>
2023-02-01 17:17:22 +0100

    update exo 2 tp 1

--- src/cameras/perspective_camera.cpp
+++ src/cameras/perspective_camera.cpp
@@ -22,19 +22,19 @@ namespace RT_ISICG
 
 	void PerspectiveCamera::_updateViewport()
 	{
-		float height = glm::tan( glm::radians(_fovy*0.5f) ) * _focalDistance;
+		float height = glm::tan( glm::radians(_fovy) ) * _focalDistance;
 		float width	 = height * _aspectRatio;
 
 
 		_viewportTopLeftCorner
-			= _position - ( _w * _focalDistance ) + ( _v * (float)( height ) ) - ( _u * (float)( width ) );
+			= _position - ( _w * _focalDistance ) + ( _v * (float)( height/2 ) ) - ( _u * (float)( width/2 ) );
 		
 
-		//float _newV = _position.x - ( width );
-		//float _newU = _position.y + ( height );
-		//float _newW = _position.z + _focalDistance;
+		float _newV = _position.x - ( width/2 );
+		float _newU = _position.y + ( height/2 );
+		float _newW = _position.z + _focalDistance;
 
-		/*_viewportTopLeftCorner = Vec3f( _newU, _newV, _newW );*/
+		_viewportTopLeftCorner = Vec3f( _newU, _newV, _newW );
 
 		_viewportU = _u * width;
 		_viewportV = _v * height;
